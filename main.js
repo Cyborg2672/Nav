db71154eb038a2cfc47f1b8df7d805c1634a1506
Cyborg2672 <64649486+Cyborg2672@users.noreply.github.com>
2020-05-12 15:46:52 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+const menu =document.querySelector(".menu");
+const navlink =document.querySelector(".nav");
+const link =document.querySelector(".nav li");
+
+
+menu.addEventListener("click",() =>{
+    navlink.classList.toggle("open")
+    
+})
